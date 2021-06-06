a564c0889f1696bd8df1350b9825e60a51ac6430
facundo <facundobistevins@gmail.com>
2021-06-06 17:44:14 -0300

    probando subir tps

---
+++ tp4_ej1.py
@@ -0,0 +1,6 @@
+def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
+
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass 
