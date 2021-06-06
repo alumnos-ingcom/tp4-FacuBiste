d37ddb01db011ad28c049d333610cba6cb4f9209
FacuBiste <83621981+FacuBiste@users.noreply.github.com>
2021-06-06 17:45:04 -0300

    Delete tp4_ej1.py

--- tp4_ej1.py
+++
@@ -1,6 +0,0 @@
-def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
-def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
-
-class IngresoIncorrecto(Exception):
-    """Esta es la Excepcion para el ingreso incorrecto"""
-    pass 
